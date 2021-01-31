69a5f58d1175178b2aa96accf8a5282940b56719
Roddie Kieley <rkieley@unifiedsoftworx.com>
2021-01-30 23:07:37 -0330

    no-issue: removed extraneous source control markers.

--- src/Commands/CommandQueue.cpp
+++ src/Commands/CommandQueue.cpp
@@ -64,11 +64,7 @@ CommandQueue::~CommandQueue()
     using namespace Poco;
 //    using namespace cms;
 
-<<<<<<< HEAD
     m_aCommandConsumer.CommandConsumedEvent -= Delegate<CommandQueue, Tuple<proton::message*, google::protobuf::Message*>*& >(this, &CommandQueue::HandleCommandConsumedEvent);
-=======
-    m_aCommandConsumer.CommandConsumedEvent -= Delegate<CommandQueue, Tuple<BytesMessage*, google::protobuf::Message*>*& >(this, &CommandQueue::HandleCommandConsumedEvent);
->>>>>>> d822ceb... no-issue: CMS decaf StlQueue -> std::queue + std::mutex.
 }
 
 // Method(s)
